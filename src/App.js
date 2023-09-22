869ffc103d664056d7c94250d70e607088ddaecf
DarkShadowXx64 <gabo.garcia.osorio@gmail.com>
2023-09-22 09:16:33 -0500

    Act y eliminacion del heather

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './index.css';
-import DashboardHeader from './components/DashboardHeader';
 import DashboardStats from './components/DashboardStats';
 import Sidebar from './components/Sidebar';
 
@@ -9,7 +8,6 @@ function App() {
     <div className="App">
       <Sidebar />
       <div className="main-content">
-        <DashboardHeader />
         <DashboardStats />
       </div>
     </div>
